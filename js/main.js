dfda70db0b143fabdf27baacbc0a310ce1f2409b
Ricardo Perez <ricardo.perez.porras@gmail.com>
2020-05-25 20:46:40 -0500

    Implementa menu móvil

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import SubMenu from './SubMenu'
+import MobileMenu from './MobileMenu'
 
-new Vue({
-    el: '.vc',
+const els = document.getElementsByClassName('vc')
+
+for (const element of els) {
+  new Vue({
+    el: element,
     components: {
-        SubMenu
+      SubMenu,
+      MobileMenu
     }
-})
+  })
+}
