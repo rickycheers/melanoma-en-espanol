bd57e8e8d030a0d73a406fae38e2d8cd5480779b
Ricardo Perez <ricpp87@gmail.com>
2020-09-21 22:45:55 -0500

    Configura API de prueba

--- api/index.js
+++ api/index.js
@@ -1,17 +1,3 @@
-const express = require('express')
-
-const app = express()
-app.use(express.json())
-
-app.post('/api/ping', function(req, res) {
-  res
-    .status(200)
-    .json({
-      body: 'pong',
-      query: req.query,
-      cookies: req.cookies,
-    })
-    .end()
-})
-
-module.exports = app
+export default (req, res) => {
+  res.json({ message: 'pong' })
+}
